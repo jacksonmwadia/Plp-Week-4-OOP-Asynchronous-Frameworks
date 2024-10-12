d5cb0be8fecd176c7519c92831af723ce4dcf1e5
jacksonmwadia <mwadiajackson@gmail.com>
2024-10-12 14:39:20 +0300

    Add Person class implementation

---
+++ person.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def introduce(self):
+        print(f"Hello, my name is {self.name}, I am {self.age} years old, and I am {self.gender}.")
+
+
+# Create an instance of the Person class
+person = Person("Johnson Mark", 30, "Male")
+
+# Call the introduce method
+person.introduce()
